8056524ab2e45c2edb27a78ba20d80db1358db20
Gustavo Esmanhotto Bareta <gustavoesmanhotto@hotmail.com>
2022-11-22 13:32:24 -0300

    feat: innitial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
+import { NextPageWithLayout } from './page';
+
+const Home: NextPageWithLayout = () => {
+  return <></>;
+};
+
+export default Home;
+
+Home.getLayout = (page) => {
+  return <PrimaryLayout>{page}</PrimaryLayout>;
+};
